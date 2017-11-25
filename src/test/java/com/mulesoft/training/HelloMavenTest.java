e438eb709856175ea08a4cd35fb2140e38d4ca4c
vsambaraju <vsambaraju@Venkats-MacBook-Pro.local>
2017-11-25 10:49:26 -0600

    add outbound property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -16,7 +16,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception { 
        MuleEvent event = runFlow("retrieveFlights"); 
-       String contentType = event.getMessage().getOutboundProperty("Content$Type"); 
+       String contentType = event.getMessage().getOutboundProperty("Content-Type"); 
        assertEquals("application/json", contentType); 
      } 
     @Override
